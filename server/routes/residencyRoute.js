2086e97d566fa95b566ff368460696c712233a93
Purnima <purnima.om.gurjar@gmail.com>
2023-09-05 15:57:47 +1000

    auth config

--- server/routes/residencyRoute.js
+++ server/routes/residencyRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
+import jwtCheck from "../config/auth0Config.js";
 import { createResidency, getAllResidency, getResidencyById } from '../controllers/residencyController.js';
 
 const router = express.Router()
 
 // route to register a user
-router.post("/create", createResidency);
+router.post("/create", jwtCheck,  createResidency);
 
 // route to get all residencies
 router.get("/all", getAllResidency);
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,19 +1,19 @@
 import express from "express";
 import { bookVisit, cancleBooking, createUser, getAllBookings, getAllFav, toFav } from "../controllers/userController.js";
-
+import jwtCheck from "../config/auth0Config.js";
 const router = express.Router()
 
 // route to register a user
-router.post("/register", createUser);
+router.post("/register", jwtCheck, createUser);
 // route to book visit
-router.post("/bookVisit/:id", bookVisit);
+router.post("/bookVisit/:id",jwtCheck, bookVisit);
 // route to get all bookings
 router.post("/allbookings", getAllBookings);
 // route to cancle booking
-router.post("/canclebooking/:id", cancleBooking);
+router.post("/canclebooking/:id",jwtCheck, cancleBooking);
 // route to add or remove from fav list
-router.post("/toFav/:rid", toFav)
+router.post("/toFav/:rid", jwtCheck, toFav)
 // route ro get all fav propertirs
-router.post("/getAllFav", getAllFav);
+router.post("/getAllFav",jwtCheck, getAllFav);
 
 export { router as userRoute}
